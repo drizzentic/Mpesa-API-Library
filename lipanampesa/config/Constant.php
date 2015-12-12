814cbe506ea36269012a8b96f1a10ae574f333d9
drizzentic <derrickrono@gmail.com>
2015-12-12 14:11:32 +0300

    Mpesa processcheckout API

--- lipanampesa/config/Constant.php
+++ lipanampesa/config/Constant.php
@@ -1,11 +1,15 @@
 <?php
-
+/**
+ * This code is Jamhuri special and it enables you to access buygoods functionality
+ * from any system build on top of php.
+ * @author Derrick Rono <derrickrono@gmail.com>
+ */
 
 /**
  * Constants for authentication
  */
-define("MERCHANT_ID", "917583"); //Put the unique merchant ID provided by the client.
-define("PASSKEY", "4ea7c2c19eff694c94dec7e5f273c1c978d154df"); //Put the passkey provided for SAG access
+define("MERCHANT_ID", ""); //Put the unique merchant ID provided by the client.
+define("PASSKEY", ""); //Put the passkey provided for SAG access
 define("URL", "https://www.safaricom.co.ke/mpesa_online/lnmo_checkout_server.php"); //Put the api endpint.
 
 /**
