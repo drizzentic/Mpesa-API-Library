0b50c355b7ff08c92ece74631161b6488cfed204
Derrick Rono <derrickrono@gmail.com>
2015-12-14 11:56:09 +0300

    Removed credentials

--- lipanampesa/config/Constant.php
+++ lipanampesa/config/Constant.php
@@ -8,8 +8,8 @@
 /**
  * Constants for authentication
  */
-define("MERCHANT_ID",'917583'); //Put the unique merchant ID provided by the client.
-define("PASSKEY", '4ea7c2c19eff694c94dec7e5f273c1c978d154df'); //Put the passkey provided for SAG access
+define("MERCHANT_ID",''); //Put the unique merchant ID provided by the client.
+define("PASSKEY", ''); //Put the passkey provided for SAG access
 define("URL", "https://www.safaricom.co.ke/mpesa_online/lnmo_checkout_server.php?wsdl"); //Put the api endpint.
 
 /**
